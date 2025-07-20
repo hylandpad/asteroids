da863af83ebfba536e1d9484cdfaae37c2b1eb68
hylandpad <hylandpad@gmail.com>
2025-07-19 22:29:08 -0400

    working triangle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import constants
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -10,13 +11,23 @@ def main():
         Screen height: {constants.SCREEN_HEIGHT}''')
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     
+    #game timer
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Initialize player
+    player= Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     #Game loop
     while True:
        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
        screen.fill("black")
-       pygame.display.flip() 
+       player.draw(screen)
+       pygame.display.flip()
+       clock.tick(60)
+       dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
